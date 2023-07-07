853782c39c17560019a72196c0a78bfe96d502ff
Sri Ram Charan marri <93866056+sriramcharanmarri@users.noreply.github.com>
2023-07-07 22:30:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let car = 0;
-function cart(){
-    car ++;
-    document.getElementById("cart").innerHTML = car;
+function cartp(){;
+        let car1=parseInt(document.getElementById("nofi").value,10);
+        if(car+car1>20){
+            alert("You can't add more than 20 items");
+        }
+        else{
+        car=car+car1;
+        document.getElementById("out").innerHTML=car;                
+        }
+}
+function cartm(){;
+    if (car>0){
+    let car1=parseInt(document.getElementById("nofi").value,10);
+    car=car-car1;
+    document.getElementById("out").innerHTML=car;
+    }
+    else{
+        alert("You have no items in your cart");
+    }
 }
